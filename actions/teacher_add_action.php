49c573c02cda184ff8e662da9e8c3dd74574f2a1
nishantmama <nisantakousiksarmah123@gmail.com>
2019-03-18 20:44:11 +0530

    vv

--- actions/teacher_add_action.php
+++ actions/teacher_add_action.php
@@ -1,10 +1,21 @@
 <?php
 require_once '../bootstrap.php';
 var_dump($_POST);
-require_once '../db.php';
 
-$sql = "INSERT into `teacher`(`name`,`email`,`deptid`,`cno`) VALUES('" . $_POST['name'] . "','" . $_POST['email'] . "','" . $_POST['deptid'] . "','" .$_POST['cno']. "')";
-$result = $db->query($sql);
+$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+    
+    $_email = $_POST['email'];
+    $_password = 'xyz';
+    $_name = $_POST['name'];
+    $_deptid = $_POST['deptid'];
+    $_cno = $_POST['cno'];
+    $_status = '0';
+    require_once '../db.php';
+
+$sql = "INSERT INTO `teacher`(`email`,`name`,`cno`,`deptid`,`password`,`status`) VALUES('" . $_email . "','" . $_name . "','" . $_cno . "','" . $_deptid . "','" . $_password . "','" . $_status . "')";
+print_r($sql);
+    $result = $conn->query($sql);
 var_dump($result);
 if ($result!=0) {
     $_SESSION['success']="successfully registered" ."<br>". "login please";
