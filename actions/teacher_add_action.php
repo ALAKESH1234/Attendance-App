78a3626e8b81d3ca6245dfee79b00bc9278d40c9
ALAKESH1234 <alasarma18@gmail.com>
2019-03-18 20:51:35 +0530

    dbds

--- actions/teacher_add_action.php
+++ actions/teacher_add_action.php
@@ -1,13 +1,19 @@
 <?php
 require_once '../bootstrap.php';
+$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
 var_dump($_POST);
 require_once '../db.php';
 
-$sql = "INSERT into `teacher`(`name`,`email`,`deptid`,`cno`) VALUES('" . $_POST['name'] . "','" . $_POST['email'] . "','" . $_POST['deptid'] . "','" .$_POST['cno']. "')";
-$result = $db->query($sql);
+$sql = "INSERT INTO `teacher` (`email`,`name`,`cno`,`deptid`,`password`,`status`) VALUES('" . $_POST['email'] . "','" . $_POST['name'] . "','" . $_POST['deptid'] . "','" .$_POST['cno']. "','" .$_POST['password']. "','" .$_POST['status']. "')";
+echo $sql;
+$result = $conn->query($sql);
+print_r($result);
 var_dump($result);
+echo $result;
 if ($result!=0) {
-    $_SESSION['success']="successfully registered" ."<br>". "login please";
+    $_SESSION['success']="successfully registered";
     header('location: ../index.php');
 }
+
 ?>
--- modals/add_teacher_modal.php
+++ modals/add_teacher_modal.php
@@ -33,7 +33,7 @@
                                         </div>
                                         <div class="form-group">
                                             <label for="exampleInputPassword1">Contact No.</label>
-                                            <input type="password" class="form-control" name="cno" placeholder="Contact No.">
+                                            <input type="text" class="form-control" name="cno" placeholder="Contact No.">
                                         </div>
                                         <div class="modal-footer">
                                         <button type="submit" class="btn btn-primary">ADD</button>
